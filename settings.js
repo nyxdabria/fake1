2d4e42d91f0579fbb52245cc20ce095008f098a0
Yuta kun <63550914+yut4kun@users.noreply.github.com>
2024-01-29 01:14:21 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://DARKAMAN:DARKAMAN@cluster0.lmfao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "evanahmed708@gmail.com" //email
+global.email_password = "babahaha@#" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
